cb0296c643e3312a9a7571fff2f7b7b2773630c1
Phillip <kip.phil@gmail.com>
2016-10-24 23:22:18 -0400

    Handle long key presses

--- computer/app.py
+++ computer/app.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pygame
 from pygame.locals import *
 from driver import Driver
@@ -23,25 +25,22 @@ class App:
                              pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
+    def on_key_press(self, key):
+        if key == pygame.K_ESCAPE:
+            print 'Quitting'
             self._running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                print 'Quitting'
-                self._running = False
-            elif event.key == pygame.K_UP:
-                print 'Key pressed up'
-                self.driver.up()
-            elif event.key == pygame.K_RIGHT:
-                print 'Key pressed right'
-                self.driver.right()
-            elif event.key == pygame.K_DOWN:
-                print 'Key pressed down'
-                self.driver.back()
-            elif event.key == pygame.K_LEFT:
-                print 'Key pressed left'
-                self.driver.left()
+        elif key == pygame.K_UP:
+            print 'Key pressed up'
+            self.driver.up()
+        elif key == pygame.K_RIGHT:
+            print 'Key pressed right'
+            self.driver.right()
+        elif key == pygame.K_DOWN:
+            print 'Key pressed down'
+            self.driver.back()
+        elif key == pygame.K_LEFT:
+            print 'Key pressed left'
+            self.driver.left()
 
     def on_loop(self):
         pass
@@ -57,10 +56,22 @@ class App:
             self._running = False
 
         while(self._running):
-            for event in pygame.event.get():
-                self.on_event(event)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                self.on_key_press(pygame.K_ESCAPE)
+            if keys[pygame.K_UP]:
+                self.on_key_press(pygame.K_UP)
+            if keys[pygame.K_RIGHT]:
+                self.on_key_press(pygame.K_RIGHT)
+            if keys[pygame.K_DOWN]:
+                self.on_key_press(pygame.K_DOWN)
+            if keys[pygame.K_LEFT]:
+                self.on_key_press(pygame.K_LEFT)
             self.on_loop()
             self.on_render()
+
+            pygame.event.pump()
+            sleep(0.05)
         self.on_cleanup()
 
 if __name__ == '__main__':
