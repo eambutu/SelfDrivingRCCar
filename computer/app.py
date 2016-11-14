ad0155c3f44c752578a3b85035679d713f2d55a8
Phillip <kip.phil@gmail.com>
2016-11-14 16:49:28 -0500

    Fix typo

--- computer/app.py
+++ computer/app.py
@@ -19,7 +19,7 @@ class App:
     def on_init(self):
         pygame.init()
 
-        self._display_surf = pygame.display.set_mod(self.size,
+        self._display_surf = pygame.display.set_mode(self.size,
                              pygame.HWSURFACE | pygame.DOUBLEBUF)
 
         self._running = True
