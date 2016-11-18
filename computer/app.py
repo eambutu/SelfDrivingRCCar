4a8e0d465d04d0a6624d80a3a700a2874b07f64a
Phillip <kip.phil@gmail.com>
2016-11-18 17:40:30 -0500

    Allow multiple inputs in app.py

--- computer/app.py
+++ computer/app.py
@@ -1,5 +1,4 @@
-from time import sleep
-
+import time
 import pygame
 from pygame.locals import *
 from driver import Driver
@@ -55,22 +54,34 @@ class App:
         if self.on_init() == False:
             self._running = False
 
+        counter = 0
         while(self._running):
+            # 4 bit integer that represents which keys were pressed
+            keys_pressed = 0
+
+            time1= time.time()
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE]:
                 self.on_key_press(pygame.K_ESCAPE)
             if keys[pygame.K_UP]:
-                self.on_key_press(pygame.K_UP)
+                keys_pressed += 4
             if keys[pygame.K_RIGHT]:
-                self.on_key_press(pygame.K_RIGHT)
+                keys_pressed += 2
             if keys[pygame.K_DOWN]:
-                self.on_key_press(pygame.K_DOWN)
+                keys_pressed += 8
             if keys[pygame.K_LEFT]:
-                self.on_key_press(pygame.K_LEFT)
+                keys_pressed += 1
+            time2 = time.time()
+            self.driver.write(keys_pressed)
+            time3 = time.time()
             self.on_loop()
             self.on_render()
 
             pygame.event.pump()
+
+            counter += 1
+            print "%d %f %f" % (counter, time2-time1, time3-time2)
+            # print str(time2 - time1) + " " + str(time3 - time2)
         self.on_cleanup()
 
 if __name__ == '__main__':
