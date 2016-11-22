d70163ff712cba42bb865f5981179b2f79bb7f51
Phillip <kip.phil@gmail.com>
2016-11-21 21:39:13 -0500

    Fix variable scope

--- computer/models/nvidiaconvnet.py
+++ computer/models/nvidiaconvnet.py
@@ -15,77 +15,75 @@ def bias_variable(shape):
 def conv2d(x, W, stride):
     return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='VALID')
 
-def get_model():
-    # Height: 144, width: 176
-    x = tf.placeholder(tf.float32, shape=[None, 144, 176, 1])
-    y_ = tf.placeholder(tf.float32, shape=[None, 4])
+# Height: 144, width: 176
+x = tf.placeholder(tf.float32, shape=[None, 144, 176, 1])
+y_ = tf.placeholder(tf.float32, shape=[None, 4])
 
-    # First convolutional layer
-    # Dimensions after: 24@70x86
-    W_conv1 = weight_variable([5, 5, 1, 24])
-    b_conv1 = bias_variable([24])
+# First convolutional layer
+# Dimensions after: 24@70x86
+W_conv1 = weight_variable([5, 5, 1, 24])
+b_conv1 = bias_variable([24])
 
-    h_conv1 = tf.nn.relu(conv2d(x, W_conv1, 2) + b_conv1)
+h_conv1 = tf.nn.relu(conv2d(x, W_conv1, 2) + b_conv1)
 
-    # Second convolutional layer
-    # Dimensions after: 36@33x41
-    W_conv2 = weight_variable([5, 5, 24, 36])
-    b_conv2 = bias_variable([36])
+# Second convolutional layer
+# Dimensions after: 36@33x41
+W_conv2 = weight_variable([5, 5, 24, 36])
+b_conv2 = bias_variable([36])
 
-    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
 
-    # Third convolutional layer
-    # Dimensions after: 48@15x19
-    W_conv3 = weight_variable([5, 5, 36, 48])
-    b_conv3 = bias_variable([48])
+# Third convolutional layer
+# Dimensions after: 48@15x19
+W_conv3 = weight_variable([5, 5, 36, 48])
+b_conv3 = bias_variable([48])
 
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 2) + b_conv3)
-    
-    # Fourth convolutional layer
-    # Dimensions after: 64@7x9
-    W_conv4 = weight_variable([3, 3, 48, 64])
-    b_conv4 = bias_variable([64])
+h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 2) + b_conv3)
 
-    h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, 1) + b_conv4)
+# Fourth convolutional layer
+# Dimensions after: 64@7x9
+W_conv4 = weight_variable([3, 3, 48, 64])
+b_conv4 = bias_variable([64])
 
-    # Fifth convolutional layer
-    # Dimensions after: 64@3x4
-    W_conv5 = weight_variable([3, 3, 64, 64])
-    b_conv5 = bias_variable([64])
+h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, 1) + b_conv4)
 
-    h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, 1) + b_conv5)
+# Fifth convolutional layer
+# Dimensions after: 64@3x4
+W_conv5 = weight_variable([3, 3, 64, 64])
+b_conv5 = bias_variable([64])
 
-    # First fully connected layer
-    # (Figure out: should I still be doing 1164? What's special about this num?)
-    h_conv5_flatten = tf.reshape(h_conv5, [-1])
-    W_fc1 = weight_variable([764, 1164]) 
-    b_fc1 = bias_variable([1164])
+h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, 1) + b_conv5)
 
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flatten, W_fc1) + b_fc1)
+# First fully connected layer
+# (Figure out: should I still be doing 1164? What's special about this num?)
+h_conv5_flatten = tf.reshape(h_conv5, [-1])
+W_fc1 = weight_variable([764, 1164]) 
+b_fc1 = bias_variable([1164])
 
-    dropout = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, dropout)
+h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flatten, W_fc1) + b_fc1)
 
-    # Second fully connected layer
-    W_fc2 = weight_variable([1164, 100])
-    b_fc2 = bias_variable([100])
+dropout = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, dropout)
 
-    h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+# Second fully connected layer
+W_fc2 = weight_variable([1164, 100])
+b_fc2 = bias_variable([100])
 
-    h_fc2_drop = tf.nn.dropout(h_fc2, dropout)
+h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-    # Third fully connected layer
-    W_fc3 = weight_variable([100, 50])
-    b_fc3 = bias_variable([50])
+h_fc2_drop = tf.nn.dropout(h_fc2, dropout)
 
-    h_fc3 = tf.nn.relu(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
+# Third fully connected layer
+W_fc3 = weight_variable([100, 50])
+b_fc3 = bias_variable([50])
 
-    h_fc3_drop = tf.nn.dropout(h_fc3, dropout)
+h_fc3 = tf.nn.relu(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
-    # Fourth fully connected layer
-    # (Again, should I be tweaking this?)
-    W_fc4 = weight_variable([50, 8])
-    b_fc3 = weight_variable([8])
+h_fc3_drop = tf.nn.dropout(h_fc3, dropout)
 
-    y_ = tf.nn.tanh(tf.matmul(h_fc3_drop, W_fc4) + b_fc4)
+# Fourth fully connected layer
+# (Again, should I be tweaking this?)
+W_fc4 = weight_variable([50, 8])
+b_fc3 = weight_variable([8])
 
+y = tf.nn.tanh(tf.matmul(h_fc3_drop, W_fc4) + b_fc4)
